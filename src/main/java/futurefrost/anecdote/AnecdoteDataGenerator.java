618398ad136d453b8cef1c3b6d01116a506f8b01
Future Frost <jay.melville2006@gmail.com>
2026-03-04 15:30:00 -0700

    Initial Project Setup

---
+++ src/main/java/futurefrost/anecdote/AnecdoteDataGenerator.java
@@ -0,0 +1,11 @@
+package futurefrost.anecdote;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AnecdoteDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
